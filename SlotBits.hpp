fb001acc0c9cd653f7b23c1dc32cd99a1be5ae55
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-09-04 14:47:20 +0200

    class SlotBits with test

---
+++ SlotBits.hpp
@@ -0,0 +1,157 @@
+//
+// Created by Andrzej Borucki on 2022-09-04
+//
+
+#ifndef CACHE_SLOTBITS_H
+#define CACHE_SLOTBITS_H
+
+#include <cstdint>
+#include <cassert>
+#include <limits>
+#include <cstring>
+#include <utility>
+#include <stdexcept>
+
+template<typename slot_t>
+class SlotBits {
+    typedef uint64_t bitType;
+    const static slot_t bitsInWord = sizeof(bitType) * 8;
+    bitType *bitmask;
+    slot_t  numBitWords;
+    slot_t capacity;
+public:
+    static slot_t limit() {
+        return std::numeric_limits<slot_t>::max()-bitsInWord;
+    }
+    explicit SlotBits(slot_t capacity):capacity(capacity){
+        if (capacity<=0)
+            throw std::range_error("capacity must be positive");
+        if (capacity>limit())
+            throw std::range_error("capacity exceeds limit "+ std::to_string(limit()));
+        numBitWords = (capacity + bitsInWord) / bitsInWord; //0 is reserved as null
+        bitmask = new bitType[numBitWords];
+        for (slot_t i=0; i < numBitWords; i++)
+            bitmask[i] = bitType(-1);
+        setAsOccupied(0);
+        for (slot_t i= capacity + 1; i < numBitWords * bitsInWord; i++)
+            setAsOccupied(i);
+    }
+    ~SlotBits(){
+        delete bitmask;
+    }
+
+    static std::pair<slot_t,slot_t> slotToPos(slot_t n) {
+        assert(n>=0 && n<=limit());
+        std::pair<slot_t,slot_t> p;
+        p.first = n / bitsInWord;
+        p.second = n % bitsInWord;
+        assert(p.first>=0 && p.second>=0 && p.second<bitsInWord);
+        p.second++;//range 1..64
+        return p;
+    }
+
+    static slot_t posToSlot(slot_t first, slot_t second) {
+        if (second==0)
+            return 0;
+        assert(first>=0 && second>0 && second<=bitsInWord);
+        second--;
+        slot_t n = first*bitsInWord + second;
+        assert(n>=0 && n<=limit());
+        return n;
+    }
+
+    static slot_t posToSlot(std::pair<slot_t,slot_t>& p) {
+        return posToSlot(p.first, p.second);
+    }
+    //slow, for test only
+    void setOccupiedRange(slot_t from, slot_t to) {
+        for (slot_t n=from; n<=to; n++)
+            setAsOccupied(n);
+    }
+
+    //for test only
+    void setAsAvailable(slot_t n) {
+        auto p = slotToPos(n);
+        setBit(bitmask[p.first], p.second);
+        assert(isSlotFree(n));
+    }
+
+    slot_t findSlotFrom(slot_t n) {
+        auto p = slotToPos(n);
+        slot_t bit_in_word = ffs_from(bitmask[p.first], p.second);
+        slot_t res = posToSlot(p.first, bit_in_word);
+        if (res>0)
+            return res;
+        for (slot_t i=p.first+1; i < numBitWords; i++)
+            if (bitmask[i])
+                return posToSlot(i, ffs_first(bitmask[i]));
+        return 0;
+    }
+
+    slot_t findSlotTo(slot_t n) {
+        auto p = slotToPos(n);
+        for (slot_t i=0; i<p.first; i++)
+            if (bitmask[i])
+                return posToSlot(i, ffs_first(bitmask[i]));
+        return posToSlot(p.first, ffs_to(bitmask[p.first], p.second));
+    }
+
+    slot_t findNextSlot(slot_t current) {
+        auto p = slotToPos(current);
+        bool lastBit = p.first == numBitWords - 1 && p.second == bitsInWord;
+        if (!lastBit) {
+            slot_t res = findSlotFrom(current+1);
+            assert(res<=0 || res>current);
+            if (res>0) return res;
+        }
+        return findSlotTo(current);
+    }
+
+    void setAsOccupied(slot_t n) {
+        auto p = slotToPos(n);
+        clearBit(bitmask[p.first], p.second);
+        assert(!isSlotFree(n));
+    }
+private:
+    static slot_t ffs_first(bitType x) {
+        return ffsll(x);
+    }
+
+    static slot_t ffs_from(bitType x, slot_t from) {
+        assert(from>0 && from <= bitsInWord);
+        bitType mask = bitType(-1) << (from-1);
+        return ffsll(x & mask);
+    }
+
+    static slot_t ffs_to(bitType x, slot_t to) {
+        assert(to > 0 && to <= bitsInWord);
+        bitType mask = ((bitType)1<< (to-1)) - 1;
+        return ffsll(x & mask);
+    }
+
+    static void clearBit(bitType& word, slot_t pos) {
+        assert(pos > 0 && pos <= bitsInWord);
+        bitType mask = ~((bitType)1 << (pos-1));
+        word &= mask;
+    }
+
+    //for test only
+    static void setBit(bitType& word, slot_t pos) {
+        assert(pos > 0 && pos <= bitsInWord);
+        bitType mask = (bitType)1 << (pos-1);
+        word |= mask;
+    }
+
+    static bool bitIsSet(bitType word, slot_t pos) {
+        assert(pos > 0 && pos <= bitsInWord);
+        bitType mask = (bitType)1 << (pos-1);
+        return (word & mask)!=0;
+    }
+
+    bool isSlotFree(slot_t n) {
+        auto p = slotToPos(n);
+        return bitIsSet(bitmask[p.first], p.second);
+    }
+};
+
+#endif //CACHE_SLOTBITS_H
---
+++ gtest_bits.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Andrzej Borucki on 2022-09-04
+//
+#include "SlotBits.hpp"
+#include "gtest/gtest.h"
+
+TEST(SlotBits, SlotPos) {
+    typedef uint8_t slot_t;
+    SlotBits<slot_t> *slb;
+    for (slot_t i=1; i<=SlotBits<slot_t>::limit(); i++) {
+        slb = new SlotBits<slot_t>(i);
+        for (slot_t j=1; j<=i; j++) {
+            auto p = slb->slotToPos(j);
+            auto jj = slb->posToSlot(p);
+            EXPECT_EQ(jj, j);
+        }
+    }
+}
+
+TEST(SlotBits, findSlotFrom) {
+    typedef uint16_t slot_t;
+    SlotBits<slot_t> slb(1023);
+    slb.setOccupiedRange(1, 1023);
+    slb.setAsAvailable(1023);//special case - at end of bitmap
+    slb.setAsAvailable(500);
+    slb.setAsAvailable(502);
+    slb.setAsAvailable(700);
+    slb.setAsAvailable(100);
+    EXPECT_EQ(slb.findSlotFrom(100),100);
+    EXPECT_EQ(slb.findSlotFrom(101),500);
+    EXPECT_EQ(slb.findSlotFrom(501),502);
+    EXPECT_EQ(slb.findSlotFrom(900),1023);
+    EXPECT_EQ(slb.findSlotFrom(1023),1023);
+    slb.setAsOccupied(1023);
+    EXPECT_EQ(slb.findSlotFrom(900),0);
+    EXPECT_EQ(slb.findSlotFrom(1023),0);
+}
+
+TEST(SlotBits, findSlotTo) {
+    typedef uint16_t slot_t;
+    SlotBits<slot_t> slb(1023);
+    slb.setOccupiedRange(1, 1023);
+    slb.setAsAvailable(1023);//special case - at end of bitmap
+    slb.setAsAvailable(500);
+    slb.setAsAvailable(502);
+    slb.setAsAvailable(700);
+    slb.setAsAvailable(100);
+    EXPECT_EQ(slb.findSlotTo(99),0);
+    EXPECT_EQ(slb.findSlotTo(100),0); //without ending range
+    EXPECT_EQ(slb.findSlotTo(101),100);
+    EXPECT_EQ(slb.findSlotTo(700),100);
+    slb.setAsOccupied(100);
+    EXPECT_EQ(slb.findSlotTo(700),500);
+}
+
+//search from current+1 with wrap
+TEST(SlotBits, findNextSlot) {
+    typedef uint16_t slot_t;
+    SlotBits<slot_t> slb(1023);
+    slb.setOccupiedRange(1, 1023);
+    EXPECT_EQ(slb.findNextSlot(500),0);
+    slb.setAsAvailable(500);
+    EXPECT_EQ(slb.findNextSlot(499),500);
+    EXPECT_EQ(slb.findNextSlot(500),0);
+    EXPECT_EQ(slb.findNextSlot(501),500);
+    slb.setAsAvailable(1023);
+    EXPECT_EQ(slb.findNextSlot(1022),1023);
+    EXPECT_EQ(slb.findNextSlot(1023),500);
+    slb.setAsOccupied(500);
+    EXPECT_EQ(slb.findNextSlot(1023),0);
+}
