2d4049eb4dd2162cadf147164c2df7273603efb4
Andrzej Borucki <borucki.andrzej@gmail.com>
2022-09-05 23:14:45 +0200

    toBaseSizeEx returns pair with 2 or 3 indicating if result is form 10000.. or 11000..

--- ResizeLogic.hpp
+++ ResizeLogic.hpp
@@ -6,25 +6,29 @@
 #define CACHE_RESIZELOGIC_H
 
 #include "BitScanner.h"
-#include <cstddef>
 
 class ResizeLogic {
-    const size_t MinSize = 16;
-public:
-    explicit ResizeLogic(size_t MinSize) : MinSize(MinSize) {}
-    size_t toBaseSize(size_t size){
+    const size_t MinSize;
+    [[nodiscard]] std::pair<size_t,int> toBaseSizeEx(size_t size) const {
         if (size<MinSize) size = MinSize;
-        if (size<=2) return 2;
+        if (size<=2) return std::make_pair(2,2);
         int bitpos = BitScanner::BitScanReverse(size);
         assert(bitpos>=1);
         bool atLeast3 = (size & (1L<<(bitpos-1) ))!=0;
-        bool serie2 = (size & (1L<<bitpos )) == size;
-        bool serie3 = (size & (3L<<(bitpos-1) )) == size;
-        if (serie2 || serie3) return size;
+        bool serie2 = (1L<<bitpos ) == size;
+        bool serie3 = (3L<<(bitpos-1) ) == size;
+        assert(!(serie2 && serie3));
+        if (serie2 || serie3) return std::make_pair(size,serie2 ? 2:3);
         if (atLeast3)
-            return 1L << (bitpos+1);
+            return std::make_pair(1L << (bitpos+1), 2);
         else
-            return 3L << (bitpos-1);
+            return std::make_pair(3L << (bitpos-1), 3);
+    }
+public:
+    explicit ResizeLogic(size_t MinSize) : MinSize(MinSize) {}
+
+    [[nodiscard]] size_t toBaseSize(size_t size) const {
+        return toBaseSizeEx(size).first;
     }
 };
 
